45f0dfb64df34d556c9c8e33f17f9036873f4543
Augusto Junior <48492543+augusto1jr@users.noreply.github.com>
2024-10-23 11:16:27 -0300

    Aula Arrays

---
+++ aula6.js
@@ -0,0 +1,69 @@
+let livros = ['javascript', 'ECMAScript', 'MongoDB'];
+
+/*
+// a consulta por index retorna o valor do elemento armazenado naquela posicao
+
+console.log(livros[0]);
+
+// a consulta por nome do elemento retorna o index, ou seja, a posicao do elemento na lista
+
+console.log(livros.indexOf('MongoDB'))
+
+// a funcao includes retorna se o valor está ou não presente na lista
+
+console.log(livros.includes('javascript'))
+
+// a funcao pop remove e retorna o ultimo elemento da lista
+
+console.log(livros.pop())
+
+// a funcao shift remove e retorna o primerio elemento da lista
+
+console.log(livros.shift())
+
+// a funcao push adiciona elementos no final da lista e retorna a quantidade de elementos na lista
+
+console.log(livros.push('python'))
+
+// a funcao unshift adiciona um ou mais elementos no inicio da lista, e retorna a quantidade de elementos na lista
+
+console.log(livros.unshift('guia de html5 e css3'))
+
+// a funcao sort ordena a lista (ordem alfabetica ou numerica crescente/decrescente)
+
+console.log(livros.sort())
+
+// a funcao length retorna o numero de elementos na lista
+
+console.log(livros.length)
+
+// Desafio sistema de estoque de frutas
+
+// remover a ultima fruta
+// adicionar uma nova fruta ao final da lista
+// mostrar o numero total de frutas na lista
+
+// a funcao splice perimite inserir ou deletar um ou mais elementos, em uma determinada posicao da lista
+
+
+livros.splice(1, 0, 'Node.js', 'Scrum') //index, nº de elementos a serem removidos, valores a serem adicionado
+
+console.log(livros)
+
+// a funcao slice permite separar as listas
+
+let n_livros = livros.length
+let corredorA1 = livros.slice(0, n_livros/2) // comeco de recorte ~ final do recorte
+let corredorA2 = livros.slice(n_livros/2, n_livros) // comeco de recorte ~ final do recorte
+
+console.log('Os livros do corredor A1 são:', corredorA1)
+console.log('Os livros do corredor A2 são:', corredorA2)
+
+// a funcao concat permite concatenar listas
+hqs = ['superman', 'x-man', 'batman', 'vingadores']
+
+livros_hqs = livros.concat(hqs)
+
+console.log(livros_hqs)
+
+*/
